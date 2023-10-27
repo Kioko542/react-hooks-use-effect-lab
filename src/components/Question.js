3317143bfa9b60251bdb8db15321a907cf56fc5b
Emmanue <kioxmanu7@gmail.com>
2023-10-27 15:15:03 +0300

    assignment done

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,31 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (timeRemaining > 1) {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      } else {
+        setTimeRemaining(0);
+        onAnswered(false);
+      }
+    }, 1000);
 
-  function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
-    onAnswered(isCorrect);
-  }
-
-  const { id, prompt, answers, correctIndex } = question;
+    // Cleanup function to clear the timer when the component unmounts
+    return () => clearTimeout(timer);
+  }, [timeRemaining, onAnswered]);
 
   return (
-    <>
-      <h1>Question {id}</h1>
-      <h3>{prompt}</h3>
-      {answers.map((answer, index) => {
-        const isCorrect = index === correctIndex;
-        return (
-          <button key={answer} onClick={() => handleAnswer(isCorrect)}>
-            {answer}
-          </button>
-        );
-      })}
-      <h5>{timeRemaining} seconds remaining</h5>
-    </>
+    <div>
+      <h2>{question.text}</h2>
+      <p>{timeRemaining > 0 ? `${timeRemaining} seconds remaining` : "Time's up!"}</p>
+    </div>
   );
 }
 
